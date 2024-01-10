1e1512e69e2fe3d6ea0f52675a0e12b772763b0f
carlos-moreno <omorenodovale@gmail.com>
2024-01-10 09:45:35 +0000

    feat: Importing the dataset to be worked on

---
+++ app.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+url = "https://raw.githubusercontent.com/alura-cursos/pandas-conhecendo-a-biblioteca/main/base-de-dados/aluguel.csv"
+
+df = pd.read_csv(url, sep=";")
+print(df)
